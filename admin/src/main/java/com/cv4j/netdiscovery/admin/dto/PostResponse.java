c3a3aa83ec7e1d9031173701cdff98f8b27e33b1
fengzhizi715 <fengzhizi715@126.com>
2018-06-12 09:29:01 +0800

    add admin module

---
+++ admin/src/main/java/com/cv4j/netdiscovery/admin/dto/PostResponse.java
@@ -0,0 +1,16 @@
+package com.cv4j.netdiscovery.admin.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PostResponse {
+
+    private int code;
+
+    private String message;
+
+    private String data;
+
+}
